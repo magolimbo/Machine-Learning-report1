edebad9b7df076f9fabe1dfe5b0777d7d65713e0
Gabriellanaro <s233541@dtu.dk>
2023-09-19 20:24:18 +0200

    commit try
    Now i'll change this

--- Movies data visualisation.py
+++ Movies data visualisation.py
@@ -16,44 +16,53 @@ doc = xlrd.open_workbook(
     r"C:\Users\Dell\Desktop\Git\Machine-Learning-report1\Movies_DS.xls").sheet_by_index(0)
 
 # extract attributes/features names
-attributeNames = doc.row_values(0, 0, 8)
+attributeNames = doc.row_values(0, 0, 10)
 print(attributeNames)
+dtypes = [('movieID', int), ('title', '<U26')]
+# dtypes = [('movieID', int), ('title', '<U26'), ('MPAA_Rating', '<U26'), ('Budget', float), ('Gross', float),
+#           ('release_date', '<U26'), ('genre', '<U26'), ('runtime', int), ('rating', float), ('rating_count', int)]
+
+# types= [int,'<U26','<U26',float,]
 
 # extract movie names
-classLabels = doc.col_values(1, 1, 636)
+moviesNames = doc.col_values(1, 1, 636)
 # with set() all the rows with the same movie title feature are removed
-classLabelsnodups = set(classLabels)
+classLabelsnodups = set(moviesNames)
 print("the original dataset has " +
-      str(len(classLabels) - len(classLabelsnodups)) + " duplicates")
+      str(len(moviesNames) - len(classLabelsnodups)) + " duplicates")
+
+X = np.empty((635,), dtype=dtypes)
+
+X['movieID'] = range(635)
+X['title'] = np.asarray(moviesNames, dtype='<U26')
+
+
+# for i, col_id in enumerate(range(4, 5)):
+#     X[:, i] = np.array(doc.col_values(col_id, 1, 637))
 
 # matrice 635 x 8 colonne
-X = np.empty((len(classLabels), len(attributeNames)))
+# X = np.empty((len(moviesNames), len(attributeNames)))
 # X[:, 0] = classLabels
 
+# Data attributes to be plotted
+# gross = 4
 
-# # MOVIES DOC
-
-# d = 3
-# e = 4
-# final_df = final_df.drop_duplicates(subset='MovieID')
 # plt.figure(1)
-# plt.plot(final_df.iloc[:, 7], final_df.iloc[:, e], 'o', color='r', alpha=0.5)
-# plt.xlabel("Rating")
-# plt.ylabel("Gross")
+# plt.plot(moviesNames, X[:, gross], 'o')
 # plt.show()
+# ##
+# # Make a simple plot of the i'th attribute against the j'th attribute
+# # Notice that X is of matrix type (but it will also work with a numpy array)
+# X = np.array(X)  # Try to uncomment this line
 
 
-# # Select the second and third columns from the DataFrame and convert
-# # them to a NumPy array
-# final_df.iloc[:, [2, 3]] = final_df.iloc[:, [
-#     2, 3]].apply(pd.to_numeric, errors='coerce')
+# # Make another more fancy plot that includes legend, class labels,
+# # attribute names, and a title.
 
-# data_subset = final_df.iloc[:, [3, 4]].values
+# plt.title('NanoNose data')
 
+# plt.xlabel("Budget")
+# plt.ylabel("Gross")
 
-# # Calculate the covariance matrix
-# cov_movies = np.cov(data_subset, rowvar=False, ddof=1,
-#                     fweights=None, aweights=None)
-
-# # Print the covariance matrix
-# print(cov_movies)
+# # Output result to screen
+# plt.show()
